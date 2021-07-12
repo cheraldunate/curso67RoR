7b3701d7435be37aa60606370e1e648da099713e
Cher aldunate <cheraldunate@gmail.com>
2021-07-12 15:33:59 -0400

    /Users/cherilynaldunate/Desktop/curso 67/

---
+++ RUBY/ruby_excercises/adivinaelnumero.rb
@@ -0,0 +1,30 @@
+=begin El usuario envía el valor correcto, devuelve "¡Lo conseguiste!".
+El usuario envía un número más alto, devuelve "¡La suposición fue demasiado alta!".
+El usuario envía un número más bajo, devuelve "¡La suposición fue demasiado baja!".
+Si quieres desafiarte, intenta utilizar unless en lugar de los condicionales if.
+=end
+
+
+
+def adivina_numero valor #miusculas y separadaspor _
+    number = 25
+    unless !(valor == number)
+        puts "valor: #{valor}. Resp: ¡Lo conseguiste!"
+    else
+        unless !(valor > number)
+            puts "valor: #{valor}. Resp: ¡La suposición fue demasiado alta!"
+        else
+            puts "valor: #{valor}. Resp: ¡La suposición fue demasiado baja!"
+        end
+    end
+end 
+
+adivina_numero 23
+adivina_numero 25
+adivina_numero 27
+
+    # tu código aquí
+
+
+
+    
---
+++ RUBY/ruby_excercises/ejercicio.rb
@@ -0,0 +1,27 @@
+no se ejecutara esta linea, es para comentario cort
+
+=begin es para comentario largo
+y para terminar
+=end 
+
+puts "hello"
+puts "coding"
+puts "dojo"
+
+
+
+x = 5
+y = 10
+z = x+y
+
+puts z
+
+#mayusculas que que se vea en consola
+BEGIN{
+    puts "this is in the beginsiempre con mayusculaigual que end block"
+
+}
+END {
+    puts "this is in the end block"
+}
+
